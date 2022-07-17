e9bc2199ac8794a4bfd18cc1c2e8bfb0b8adc299
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-07-17 19:29:15 +0600

    Separated Startup Folders and Added Start Menu (All Users)

--- launcher.bat
+++ launcher.bat
@@ -2,9 +2,11 @@
 ECHO Open Host File = 1
 ECHO Open Office Addins Folder = 2
 ECHO Open SentTo Folder = 3 
-ECHO Open Start Menu Folder = 4
-ECHO Open Startup Folders = 5
-ECHO Open Microsoft Word Startup Folder = 6
+ECHO Open Start Menu Folder (Current User) = 4
+ECHO Open Start Menu Folder (Current User) = 5
+ECHO Open Startup Folder (Current User) = 6
+ECHO Open Startup Folder (All Users) = 7
+ECHO Open Microsoft Word Startup Folder = 8
 :start
 SET choice=
 SET /p choice=Choose Folder To Open [1-6]: 
@@ -15,6 +17,7 @@ IF '%choice%'=='3' GOTO 3
 IF '%choice%'=='4' GOTO 4
 IF '%choice%'=='5' GOTO 5
 IF '%choice%' == '6' GOTO 6
+IF '%choice%' == '6' GOTO 7
 ECHO "%choice%" is not valid
 ECHO.
 GOTO start
@@ -37,10 +40,17 @@ explorer.exe %AppData%\Microsoft\Windows\Start Menu
 exit
 
 :5
+explorer.exe C:\ProgramData\Microsoft\Windows\Start Menu
+exit
+
+:6
 explorer.exe %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+exit
+
+:7
 explorer.exe C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 exit
 
-:6 
+:8
 explorer.exe %AppData%\Microsoft\Word\STARTUP
 exit
